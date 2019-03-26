152386904eb12352786ae494b5cc83c3f3d6e7bf
austinin <austinin@rose-hulman.edu>
2019-03-26 14:41:13 -0400

    Made script better

--- Future World/Assets/StandardBot.cs
+++ Future World/Assets/StandardBot.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class StandardBot : MonoBehaviour { 
 
     private float movementSpeed = 25f;
+    private float rotationSpeed = 500f;
     private int health = 15;
-
-    Rigidbody rigidbody;
 
     // Start is called before the first frame update
     void Start() {
-        rigidbody = GetComponent<Rigidbody>();
 
     }
 
@@ -24,24 +22,16 @@ public class StandardBot : MonoBehaviour {
             }
             else if (Input.GetKey(KeyCode.S))
             {
-                rigidbody.position += Vector3.back * Time.deltaTime * movementSpeed;
-            }
-            else if (Input.GetKey(KeyCode.A))
-            {
-                rigidbody.position += Vector3.left * Time.deltaTime * movementSpeed;
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                rigidbody.position += Vector3.right * Time.deltaTime * movementSpeed;
+                transform.position += -transform.forward * Time.deltaTime * movementSpeed;
             }
 
             if (Input.GetKey(KeyCode.E))
             {
-                transform.Rotate(0, Time.deltaTime * 500.0f, 0);
+                transform.Rotate(0, Time.deltaTime * rotationSpeed, 0);
             }
             else if (Input.GetKey(KeyCode.Q))
             {
-                transform.Rotate(0, Time.deltaTime * -500.0f, 0);
+                transform.Rotate(0, Time.deltaTime * -rotationSpeed, 0);
             }
         }
     }
