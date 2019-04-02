1e4bafa512536c10dc84b79d240648ef678d7c5d
Brock Grinstead <brockgrinstead@gmail.com>
2019-04-02 13:35:32 -0400

    Security Robot with projectile detection

---
+++ Future World/Assets/SecurityRobotFolder/_Scripts/Despawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Despawn : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public float despawnTime = 120;
+
+    int internalTimer = 0;
+
+    void Start()
+    {
+        
+    }
+    
+    void FixedUpdate()
+    {
+        internalTimer++;
+        if(internalTimer == despawnTime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
