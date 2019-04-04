6c75ed87d9c968d7a750ef288563c1f011d56ef9
austinin <austinin@rose-hulman.edu>
2019-04-04 14:08:40 -0400

    Changed SB to detect raycast hits

--- Future World/Assets/StandardBot/_Scripts/StandardBot.cs
+++ Future World/Assets/StandardBot/_Scripts/StandardBot.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StandardBot : MonoBehaviour {
+public class StandardBot : MonoBehaviour, Target {
 
     [Header("Set in Inspector")]
     public GameObject me;
 
-    protected int health = 15;
+    protected float health = 15f;
     protected float movementSpeed = 2f;
 
     public void DealDamage(int damage)
@@ -33,6 +33,27 @@ public class StandardBot : MonoBehaviour {
         {
             Destroy(go);
             DealDamage(10);
+        }
+    }
+
+    public float Health
+    {
+        get { return this.health; }
+        set { this.health = value; }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void takeDamage(float amount)
+    {
+        Debug.Log("Standard Bot took " + amount + " damage");
+        this.health -= amount;
+        if (this.health <= 0)
+        {
+            Die();
         }
     }
 }
