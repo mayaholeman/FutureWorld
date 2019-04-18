0cb2f789083288045d66fdb49743c2026c1eb228
Brock Grinstead <brockgrinstead@gmail.com>
2019-04-18 11:50:11 -0400

    Changed Secuirty robot to use target interface, does not yet shoot however. Also decomposed the security robot asset into individual parts to use for the assembling new robot.

--- Future World/Assets/SecurityRobotFolder/_Scripts/SecurityRobot.cs
+++ Future World/Assets/SecurityRobotFolder/_Scripts/SecurityRobot.cs
@@ -2,12 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SecurityRobot : MonoBehaviour
+public class SecurityRobot : MonoBehaviour, Target
 {
+
+	[Header("Set in Inspector")]
+	//public GameObject me;
+    
+    //Target variables
+	private float health = 65f;
+
+    //Movement variables
     Transform player;                   // Reference to the player's position.
     UnityEngine.AI.NavMeshAgent nav;    // Reference to the nav mesh agent.
     Animator anim;                      // Reference to the animator component.
 
+    //Shooting variables
+    public float shootingRange = 100f;
+    GameObject robotGun;
+
+    GameObject GetChildWithName(GameObject obj, string name)
+    {
+        Transform trans = obj.transform;
+        Transform childTrans = trans.Find(name);
+        if (childTrans != null)
+        {
+            return childTrans.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
 
     void Awake()
     {
@@ -15,8 +40,8 @@ public class SecurityRobot : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player").transform;
         nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
         anim = GetComponent<Animator>();
+        robotGun = GetChildWithName(this.gameObject, "AssualtRifle");
     }
-
 
     void FixedUpdate()
     {
@@ -36,6 +61,41 @@ public class SecurityRobot : MonoBehaviour
             // Tell the animator whether or not the robot is moving.
             anim.SetBool("IsMoving", false);
         }
-        
+
+    }
+
+    private void Shoot(GameObject gun, float damage)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(gun.transform.position, gameObject.transform.forward, out hit, shootingRange))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.takeDamage(damage);
+            }
+        }
+    }
+
+    public float Health
+    {
+        get { return this.health; }
+        set { this.health = value; }
+    }
+
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void takeDamage(float amount)
+    {
+        Debug.Log("Security Bot took " + amount + " damage");
+        this.health -= amount;
+        if (this.health <= 0)
+        {
+            Die();
+        }
     }
 }
